7b897217669dcc4f45470bc0d5d7221a892a000b
Peter Bridgland <peter.bridgland@bjss.com>
2023-03-17 09:22:10 +0000

    refine movement system add whichRoom method

--- game.py
+++ game.py
@@ -1,34 +1,45 @@
 class Player:
-    def __init__(self, name, position):
+    def __init__(self, name, position, building):
         self.name = name
-        self.position_x = position[0]
-        self.position_y = position[1]
+        self.position = position
+        self.building = building
     
     def move(self, direction):
         movement = (0,0)
         match direction:
             case "w":
-                movement = (-1,0)
+                movement = (-1, 0)
             case "a":
-               movement = (0,-1)
+               movement = (0, -1)
             case "s":
-                movement = (1,0)
+                movement = (1, 0)
             case "d":
-                movement = (0,1)
+                movement = (0, 1)
             case _:
                 pass #error handle invalid input
-        self.position_x += movement[0]
-        self.position_y += movement[1]
+        newRow = (self.position[0] + movement[0])
+        newCol = (self.position[1] + movement[1])
+        newPosition = (newRow, newCol)
+        try:
+            self.building.whichRoom(newPosition)
+        except:
+            print("bounced")
+        else:
+           self.position = newPosition 
+        
 
 
 class Building:
     def __init__(self):
         self.rooms = {
-        (0,0) : Room("kitchen"), (0,1) : Room("hallway"),
-        (1,0) : Room("hallway"), (1,1) : Room("hallway"), (1,2) : Room("hallway"), (1,3) : Room("hallway"),
-                                 (2,1) : Room("hallway"),
-                                 (3,1) : Room("bedroom"),
+        (0,0) : Room("kitchen"),      (0,1) : Room("north hallway"),
+        (1,0) : Room("west hallway"), (1,1) : Room("central hallway"), (1,2) : Room("semi-east hallway"), (1, 3) : Room("east hallway"),
+                                      (2,1) : Room("south hallway"),
+                                      (3,1) : Room("bedroom"),
         }
+
+    def whichRoom(self, position):
+        return self.rooms[position].name
 
 
 class Room:
@@ -44,13 +55,18 @@ class Item:
 
 
 mansion = Building()
+player  = Player("Bob", (0,0), mansion)
 
-# print(mansion.rooms[(4,1)])
 
-player  = Player("Bob", (0,0))
-
-print(f"({player.position_x},{player.position_y})")
-
+print(f"{mansion.whichRoom(player.position)}")  #kitchen
 player.move("s")
+print(f"{mansion.whichRoom(player.position)}")  # w h
+player.move("s")                                # bounced
+print(f"{mansion.whichRoom(player.position)}")  # w h 
+player.move("d")
+print(f"{mansion.whichRoom(player.position)}")  # c h 
+player.move("w")
+print(f"{mansion.whichRoom(player.position)}")  # n h 
+player.move("w")                                # bounced 
+print(f"{mansion.whichRoom(player.position)}")  # n h
 
-print(f"({player.position_x},{player.position_y})")
