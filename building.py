df59ed814201cf116e9e42199ac0284747ef225b
Peter Bridgland <peter.bridgland@bjss.com>
2023-04-14 16:23:27 +0100

    add floors, refine picking up

--- building.py
+++ building.py
@@ -16,13 +16,20 @@ class Building:
         return self.where(position).name
     
     def itemsInRoom(self, position):
-        self.where(position).print_items()
-        return self.where(position).items
+        room = self.where(position)
+        room.print_items()
+        items = []
+        for location in room.items:
+            item = room.items[location]
+            if item != None:
+                items.append(item)
+        return items
 
 class Room:
     def __init__(self, name):
         self.name = name 
         self.items = {
+            "on the floor": None
         }
 
     def add_item(self, location, name):
@@ -31,17 +38,24 @@ class Room:
     def take_item(self, name): 
         for location in self.items: 
             item = self.items[location]
+            if item == None:
+                continue
             if item.name == name :
                 print(f'you take the {name} from {location}')
-                return self.items.pop(location)
+                self.items[location] = None 
+                return item
         print(f'you could not find the {name}')
 
     def place_item(self, item, location):
         self.items.update({location : item})
 
     def print_items(self):
-        for item in self.items:
-            print(f'there is a {self.items[item].name} {item}')
+        for location in self.items:
+            item = self.items[location] 
+            if item != None: 
+                print(f'there is a {item.name} {location}')
+            else: 
+                print(f"there is nothing {location}")
 
 
 
