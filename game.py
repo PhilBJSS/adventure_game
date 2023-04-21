109bb66d38231ccf3f8f416afb1448df72fa9f97
phil_beal <philip.beal@bjss.com>
2023-04-21 16:40:55 +0100

    implement room item randomisation

--- game.py
+++ game.py
@@ -1,31 +1,65 @@
+import random
 from player import Player
 from building import Building
 from items import Item
 
-def game_loop():
+item_names = [
+'spoon',
+'flower vase',
+'potato',
+'letter opener',
+'chocolate bar',
+'pillow',
+'bust of William Shakespeare',
+'cheese',
+'laptop',
+'key',
+'lead pipe',
+'wine glass'
+]
 
-    while (True):
-        print(f"{mansion.whichRoom(player.position)}")  #kitchen
-        hasFoundItems = player.look_around()
-        if hasFoundItems: 
-            itemToTake = input("Enter item to pick up: ")
-            player.pick_up(itemToTake)
-        player.check_inventory()
-        if len(player.items)>0:
-            itemToPlace = input("Enter item to put down: ")
-            player.put_down(itemToPlace)
-            player.check_inventory()
-        if mansion.whichRoom(player.position) == 'west hallway':
-            break
-        movechar = input("Enter a direction: ")
-        player.move(movechar)
+location_names = [
+    'on the table',
+    'inside the cupboard',
+    'on the wall',
+    'underneath the sofa',
+    'beside the clock',
+    'taped to the ceiling',
+    'on the window sill',
+    'by the cat',
+]
+
+class Game:
+    def __init__(self):
+        self.mansion = Building()
+        self.player  = Player("Bob", (0,0), self.mansion)
+        room_coordinates = list(self.mansion.rooms)
+        for i in range(5):
+            room_coordinate =  random.choice(room_coordinates)
+            self.mansion.rooms[room_coordinate].add_item(random.choice(location_names), random.choice(item_names))
+
+    def play(self):
+
+        while (True):
+            print(f"{self.mansion.whichRoom(self.player.position)}")  #kitchen
+            hasFoundItems = self.player.look_around()
+            if hasFoundItems: 
+                itemToTake = input("Enter item to pick up: ")
+                self.player.pick_up(itemToTake)
+            self.player.check_inventory()
+            if len(self.player.items)>0:
+                itemToPlace = input("Enter item to put down: ")
+                self.player.put_down(itemToPlace)
+                self.player.check_inventory()
+            if self.mansion.whichRoom(self.player.position) == 'west hallway':
+                break
+            movechar = input("Enter a direction: ")
+            self.player.move(movechar)
 
 
 if __name__ == "__main__":
-    mansion = Building()
-    player  = Player("Bob", (0,0), mansion)
-    mansion.rooms[(0,1)].add_item('on the table', 'flower vase')
-    game_loop()
+    game = Game()
+    game.play()
 
 
 
